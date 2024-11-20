1c7727e3e4012bcf3d8393e096f6c0f9f68580ec
Grace <64219828+graceful-one@users.noreply.github.com>
2024-11-20 21:40:58 +0000

    i dont remember what i edited

--- Home.py
+++ Home.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd 
 
-st.page_link("your_app.py", label="Dolphin Query")
-st.page_link("photo_query.py", label="Picture app")
-#st.page_link("pages/page_2.py", label="update data")
-
 #import geopandas as gpd 
 st.title("Home Page")
 
+st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣") 
