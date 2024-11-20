3fee9a5128eb364346b1a6a524d2aa1134616410
Grace <64219828+graceful-one@users.noreply.github.com>
2024-11-20 21:42:33 +0000

    home setup

--- Home.py
+++ Home.py
@@ -4,5 +4,5 @@ import pandas as pd
 #import geopandas as gpd 
 st.title("Home Page")
 
-st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
-st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣") 
+st.page_link("pages/Dolphin_query.py", label="dolphin query", icon="1️⃣")
+st.page_link("pages/photo_query.py", label="photo query", icon="2️⃣") 
