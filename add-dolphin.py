96cabf3fcbf210cc1bee2e31e71aa4515a126cc0
Alejandra Rios <124205497+alerios3@users.noreply.github.com>
2024-12-06 16:41:12 -0500

    Update add-dolphin.py

--- add-dolphin.py
+++ add-dolphin.py
@@ -3,21 +3,22 @@ import os
 import streamlit as st
 import datetime
 import pandas as pd
-from openpyxl import load_workbook
 import uuid
 
 #load existing dolphin observation dataset
-file_path = './tblDolphin.xlsx'
+file_path = './tblDolphin.csv'
 
 #load excel file
-try:
-    existing_data = pd.read_excel(excel_file) #from pandas, used to read file
-except Exception as e: #from python to handle errors
-    st.error(f"Error loading the Excel file: {e}") #function from streamlit for errors
-    existing_data = None #if file is missing or invalid, program does not crash
+if os.path.exists(file_path):
+    existing_data = pd.read_csv(file_path) #from pandas, used to read file
+else:
+    exisiting_data = pd.DataFrame(columns=[
+        "Trip #", "Sight #", "Dolphin ID Number", "New Dolphin?", "Resight?",
+        "Seen Offshore?", "Clarity", "Contrast", "Angle", "Partial",
+        "Overall", "Distinctiveness", "Verified"])
 
 #set up streamlit app
-st.title("Dolphin Observation Data Entry)
+st.title("Dolphin Observation Data Entry")
 st.write("Add new observation data to the database.")
 
 #create a form 
@@ -26,7 +27,7 @@ with st.form(key="data_entry_form"):
   #autonum
   #trip_date = st.date_input(format="YYYY/MM/DD") #https://docs.streamlit.io/develop/api-reference/widgets/st.date_input
   #trip_time = st.time_input() #https://docs.streamlit.io/develop/api-reference/widgets/st.time_input
-  trip_num = st.text_input("Trip #) #https://docs.streamlit.io/develop/api-reference/widgets/st.time_input
+  trip_num = st.text_input("Trip #") #https://docs.streamlit.io/develop/api-reference/widgets/st.time_input
   sight_num = st.number_input("Sight #") #https://docs.streamlit.io/develop/api-reference/widgets/st.number_input
   dolphin_id_num = st.number_input("Dolphin ID Number")
   new_dolphin = st.checkbox("New Dolphin?") #https://docs.streamlit.io/develop/api-reference/widgets/st.checkbox
@@ -40,4 +41,27 @@ with st.form(key="data_entry_form"):
   overall = st.number_input("Overall")
   distinctiveness = st.number_input("Distinctiveness")
   verified = st.checkbox("Verified")
+  }
 
+if submitted:
+    new_row = { #dictionary
+        "Trip #": trip_num,
+        "Sight #": sight_num,
+        "Dolphin ID Number": dolphin_id_num,
+        "New Dolphin?": new_dolphin,
+        "Resight?": resight,
+        "Seen Offshore?": seen_offshore,
+        "Clarity": clarity,
+        "Contrast": contrast,
+        "Angle": angle,
+        "Partial": partial,
+        "Overall": overall,
+        "Distinctiveness": distinctiveness,
+        "Verified": verified,
+    }
+
+updated_data = existing_data.append(new_row, ignore_index=True) #append new row 
+
+updated_data.to_csv(file_path, index=False) #save updates
+
+st.success("Observation added!") #confirm data addition
