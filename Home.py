1dcce2775cd969355b983cb676eea1b18aa8b4e9
Hopefullyxr <156842391+Hopefullyxr@users.noreply.github.com>
2024-12-12 14:41:57 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,5 +4,5 @@ import pandas as pd
 #import geopandas as gpd 
 st.title("Home Page")
 
-st.page_link("pages/Dolphin_query.py", label="dolphin query", icon="1️⃣")
-st.page_link("pages/photo_query.py", label="photo query", icon="2️⃣") 
+st.page_link("pages/SearchADolphin.py", label="Find A Dolphin🐬", icon="1️⃣")
+st.page_link("pages/photo_query.py", label="Gallery", icon="2️⃣") 
