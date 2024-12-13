80b738e3e694ffbd15fdeeaf4fc259f8fb7c2b16
Grace <64219828+graceful-one@users.noreply.github.com>
2024-12-12 19:57:26 -0500

    Update map_a_dolphin.py

--- pages/map_a_dolphin.py
+++ pages/map_a_dolphin.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-tblDolphin = 'data/tblDolphin.csv' 
-df = pd.DataFrame(tblDolphin) #convert data into a dataframe
+pd.read_csv(r"data/tblDolphin.csv", encoding="ISO-8859-1") #convert data into a dataframe
 st.title("Dolphin Sightings Viewer") #streamlit title
 
 dolphins = df["Dolphin_Name"].unique() #streamlit dropdown 
