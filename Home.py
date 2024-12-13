d6778e4400fb157a356f649bb4251ca812cfe0a9
Grace <64219828+graceful-one@users.noreply.github.com>
2024-12-12 20:28:09 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,11 +4,12 @@ import pandas as pd
 #import geopandas as gpd 
 st.title("Home Page")
 
-st.page_link("pages/Search_a_Dolphin.py", label="Find A Dolphin🐬", icon="1️⃣")
+st.page_link("pages/1_Search_a_Dolphin.py", label="Find A Dolphin🐬", icon="1️⃣")
 st.markdown("<p style='color:grey; margin-left: 100px;'>---Find information by dolphin ID</p>", unsafe_allow_html=True)
+
+st.page_link("pages/3_add_a_dolphin.py", label="Add A Dolphin", icon="3️⃣") 
+st.markdown("<p style='color:grey; margin-left: 100px;'>---Add your dolphin observations</p>", unsafe_allow_html=True)
 
 st.page_link("pages/photo_query.py", label="Gallery", icon="2️⃣") 
 st.markdown("<p style='color:grey; margin-left: 100px;'>---If you only looking for the fin photo</p>", unsafe_allow_html=True)
 
-st.page_link("pages/add_a_dolphin.py", label="Add A Dolphin", icon="3️⃣") 
-st.markdown("<p style='color:grey; margin-left: 100px;'>---Add your dolphin observations</p>", unsafe_allow_html=True)
